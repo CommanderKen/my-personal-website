626416a299d6384217721ab0839e568c51ac7e1c
CommanderKen <commander_keen01@proton.me>
2023-12-08 17:56:01 +0100

    Added Contacts page

---
+++ src/components/pages/Contacts.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { GoMail } from "react-icons/go";
+import '../../App.css';
+import './Contacts.css';
+
+function Contacts() {
+  return (
+    <div className='background'>
+      <div className='contacts-container'>
+        <div className='contacts-card'>
+          <h1 className='contacts-title'>You can find me on:</h1>
+          <div className='contacts-description'>
+            <span><GoMail className='mail-icon' /></span>
+            <a href="mailto:example@mail.com" className='link'>example@mail.com</a>
+          </div>
+          <div className='contacts-description'>
+            <span>
+              <img src="/images/github-mark-white.svg" alt="Github" className='github-icon' />
+            </span>
+            <a href="https://github.com/..." className='link'>github.com/...</a>
+          </div>
+          <div className='contacts-description'>
+            <span>
+              <img src="/images/linkedin.png" alt="Linkedin" className='linkedin-icon' />
+            </span>
+            <a href="https://www.linkedin.com/in/..." className='link'>linkedin.com/in/...</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contacts;
